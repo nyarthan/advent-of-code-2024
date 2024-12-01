f0c235431836c34262b22a1b1efac487da91cbe2
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-12-01 13:25:04 +0100

    feat: day 01 part 2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,14 +1,15 @@
-pub fn solution() -> u32 {
+use std::collections::HashMap;
+
+pub fn solution() -> i32 {
     let input = include_str!("input");
     let (first, second): (Vec<_>, Vec<_>) = input.split_whitespace().enumerate().partition(|(i, _)| i % 2 == 0);
-    let mut first: Vec<i32> = first.into_iter().map(|(_, s)| s.parse().unwrap()).collect();
-    let mut second: Vec<i32> = second.into_iter().map(|(_, s)| s.parse().unwrap()).collect();
+    let first: Vec<i32> = first.into_iter().map(|(_, s)| s.parse().unwrap()).collect();
+    let second: Vec<i32> = second.into_iter().map(|(_, s)| s.parse().unwrap()).collect();
 
-    first.sort();
-    second.sort();
+    let frequency = second.into_iter().fold(HashMap::new(), |mut map, i| {
+        *map.entry(i).or_insert(0) += 1;
+        map
+    });
 
-    let differences: Vec<u32> = first.iter().zip(second.iter()).map(|(f, s)| f.abs_diff(*s)).collect();
-    let difference: u32 = differences.iter().sum();
-
-    difference
+    first.into_iter().map(|i| i * frequency.get(&i).unwrap_or(&0)).sum()
 }
